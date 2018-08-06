e0078bacbcdcd6d7aff6bd1a065caaf11c94a6e8
amonfort808 <anthony.monfort808@gmail.com>
2018-08-05 15:52:49 -1000

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by anthony monfort on 8/5/18.
+//  Copyright © 2018 Anthony Monfort. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
